09af62d4a24f975211c33e956c694c59d1d8cde7
LeAnne B <branchleannelmb@gmail.com>
2023-05-29 16:12:21 -0400

    adds in 5 day forecast

--- assets/scripts/playpen.js
+++ assets/scripts/playpen.js
@@ -5,3 +5,22 @@
         //console.log(data.list[0].dt * 1000)
         //var entryDate = (new Date(data.list[0].dt * 1000));
         //console.log(entryDate).toLocaleString("en-US", "short");
+
+        // so, to truly get the next day, we need in our loop to add a number and then pull the item where dt === the result that truly makes it the next day? 
+        //Orrrr 
+
+                //Note that we are only returned 40 entries which is 8 x 5 so starting at 0...
+        
+        // var windSpeedElement = document.createElement("p");
+        //set id to plus i like answers
+        // windSpeedElement.innerText = "Wind Speed: " + windSpeedFromAPI + " meters per second";
+
+        //PLACING ELEMENTS
+        //document.getElementById("day" + i).innerText = "Test" + i;
+        // document.getElementById("day" + i).appendChild(currentCityElement + i);
+        // document.getElementById("day" + i).appendChild(currentDateElement + i);
+        // document.getElementById("day" + i).appendChild(currentWeatherIcon + i);
+        // document.getElementById("day" + i).appendChild(currentConditionElement + i);
+        // document.getElementById("day" + i).appendChild(temperatureElement + i);
+        // document.getElementById("day" + i).appendChild(humidityElement + i);
+        // document.getElementById("day" + i).appendChild(windSpeedElement + i);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -8,8 +8,11 @@ function getApi() {
 
   //also add in way to handle invalid location
 
+  
+
     var cityName = document.querySelector("#inputbox").value;
     var APIKey = "60ebe634619c5700bf67dc2646a55408";
+    //please see code further below where I am taking the city name and getting the lat/long before running a new API request with lat and long
     var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=' + APIKey;
   
     fetch(requestUrl)
@@ -54,41 +57,68 @@ function getWeatherDataByLatandLong(latitude, longitude) {
       .then(function (data) {
         // Use the console to examine the response
         // console.log(data);
+        console.log(data.list)
 
-        //clears current condition box in case info from previous search is there
-        document.getElementById("currentcond").innerHTML = "";
+        //clears current day boxes if info from previous search is there
+        document.getElementById("day0").innerHTML = "";
+        document.getElementById("day1").innerHTML = "";
+        document.getElementById("day2").innerHTML = "";
+        document.getElementById("day3").innerHTML = "";
+        document.getElementById("day4").innerHTML = "";
+        document.getElementById("day5").innerHTML = "";
 
+
+        //Now to fill in 5 day forecast
+        //console.log(data.list);
+
+        //According to the weather api documentation, it pull entries every 3 hours which is 10800 added to the UNIX time (dt)
+        //since 24 hrs divided by 3 is 8...so every 8th item is what we need actually
+        //since this api returns 40 items starting at 0, maybe add 7? 
+
+        var numberOfDaysToForeCast = 5;
+        var objectLocationCounter = 0; //used to grab data every 8th position
+  
+        //var startingDayNumber = 1;
+        //made a div for each day
+        for (var i = 0; i < (numberOfDaysToForeCast + 1); i++) {
+
+        //GETTING DATA
+        //Windspeed
+        // var windSpeedFromAPIforForecastedDay = data.list[objectLocationCounter].wind.speed;
+        // console.log(windSpeedFromAPIforForecastedDay);
+
+        
         //Gets City Name
         // console.log(data.city.name);
         var currentCityFromAPI = data.city.name;
         var currentCityElement = document.createElement("p");
         currentCityElement.innerText = "City: " + currentCityFromAPI;
-        document.getElementById("currentcond").appendChild(currentCityElement);
+        document.getElementById("day" + i).appendChild(currentCityElement);
 
         //Gets Date
         // console.log(data.list[0].dt_txt);
-        var currentDateFromAPI = data.list[0].dt_txt;
+        var currentDateFromAPI = data.list[objectLocationCounter].dt_txt;
         var currentDateElement = document.createElement("p");
         currentDateElement.innerText = "Date: " + currentDateFromAPI.slice(0, 11); //chops off utc timestamp at end to make this just the date
-        document.getElementById("currentcond").appendChild(currentDateElement);
+        document.getElementById("day" + i).appendChild(currentDateElement);
 
         //Gets weather description
         //see https://openweathermap.org/weather-conditions
         // console.log(data.list[0].weather[0].description);
-        var iconNum = data.list[0].weather[0].icon
+        var iconNum = data.list[objectLocationCounter].weather[0].icon
         var iconUrl = 'https://openweathermap.org/img/wn/' + iconNum + "@2x.png";
         var currentWeatherIcon = document.createElement("img");
         currentWeatherIcon.src = iconUrl;
-        document.getElementById("currentcond").appendChild(currentWeatherIcon);
+        document.getElementById("day" + i).appendChild(currentWeatherIcon);
 
         //in text in case icon does not load
-        var currentConditionFromAPI = data.list[0].weather[0].description;
+        var currentConditionFromAPI = data.list[objectLocationCounter].weather[0].description;
         var currentConditionElement = document.createElement("p");
         currentConditionElement.innerText = currentConditionFromAPI.toUpperCase();
-        document.getElementById("currentcond").appendChild(currentConditionElement);
+        document.getElementById("day" + i).appendChild(currentConditionElement);
 
         //Temperature
-        var TempInKelvinFromAPI = data.list[0].main.temp;
+        var TempInKelvinFromAPI = data.list[objectLocationCounter].main.temp;
         // console.log(typeof(TempInKelvinFromAPI)); confirms this is returned as number
         //CONVERT TO FAHRENHEIT
         var TempinFahren = (TempInKelvinFromAPI - 273.15) * 9/5 + 32; // thank you google for the conversion formula!
@@ -101,55 +131,28 @@ function getWeatherDataByLatandLong(latitude, longitude) {
         //console.log("Temp: " + cleanTempinFahren + "°F / " + cleanTempinCel + "°C");
         var temperatureElement = document.createElement("p");
         temperatureElement.innerText = "Temp: " + cleanTempinFahren + "°F / " + cleanTempinCel + "°C";
-        document.getElementById("currentcond").appendChild(temperatureElement);
+        document.getElementById("day" + i).appendChild(temperatureElement);
 
         //Humidity
         //console.log(data.list[0].main.humidity);
-        var humidityFromApi = data.list[0].main.humidity;
+        var humidityFromApi = data.list[objectLocationCounter].main.humidity;
         var humidityElement = document.createElement("p");
         //in percent
         humidityElement.innerText = "Humidity: " + humidityFromApi + "%";
-        document.getElementById("currentcond").appendChild(humidityElement);
+        document.getElementById("day" + i).appendChild(humidityElement);
 
         //Wind Speed
         // console.log(data.list[0]);
         //console.log(data.list[0].wind.speed);
         // console.log(data.list)
-        var windSpeedFromAPI = data.list[0].wind.speed;
+        var windSpeedFromAPI = data.list[objectLocationCounter].wind.speed;
         var windSpeedElement = document.createElement("p");
         windSpeedElement.innerText = "Wind Speed: " + windSpeedFromAPI + " meters per second";
-        document.getElementById("currentcond").appendChild(windSpeedElement);
+        document.getElementById("day" + i).appendChild(windSpeedElement);
 
-        //Now to fill in 5 day forecast
-        //console.log(data.list);
 
-        //according to the weather api documentation, it pull entries every 3 hours which is 10800 added to the UNIX time (dt)
-        //so, to truly get the next day, we need in our loop to add a number and then pull the item where dt === the result that truly makes it the next day? 
-        //Orrrr since 24 hrs divided by 3 is 8...so every 8th item is what we need actually
-        //
 
-        var numberOfDaysToForeCast = 5;
-        //var startingDayNumber = 1;
-        //made a div for each day
-        for (var i = 1; i < (numberOfDaysToForeCast + 1); i++) {
-
-        //GETTING DATA
-
-        // var windSpeedFromAPI = data.list[i].wind.speed;
-        // var windSpeedElement = document.createElement("p");
-        //set id to plus i like answers
-        // windSpeedElement.innerText = "Wind Speed: " + windSpeedFromAPI + " meters per second";
-
-          //PLACING ELEMENTS
-          //document.getElementById("day" + i).innerText = "Test" + i;
-          // document.getElementById("day" + i).appendChild(currentCityElement + i);
-          // document.getElementById("day" + i).appendChild(currentDateElement + i);
-          // document.getElementById("day" + i).appendChild(currentWeatherIcon + i);
-          // document.getElementById("day" + i).appendChild(currentConditionElement + i);
-          // document.getElementById("day" + i).appendChild(temperatureElement + i);
-          // document.getElementById("day" + i).appendChild(humidityElement + i);
-          // document.getElementById("day" + i).appendChild(windSpeedElement + i);
-
+        objectLocationCounter = objectLocationCounter + 7;
           
         }
 
